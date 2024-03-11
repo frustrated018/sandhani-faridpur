bc1ed71de1f1c364a81b303502e6aa3a415da1a1
frustrated018 <niloyd018@gmail.com>
2024-03-11 23:34:52 +0600

    Dropdown menu update
    Added a state for user to display various user related activites
    used sonner to diplay relavent toasts based on the user state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sandhani | Faridpur Branch",
-  description: "Faridpur branch of the largest sudent led Blood bank in bangladesh, Sandhani",
+  description:
+    "Faridpur branch of the largest sudent led Blood bank in bangladesh, Sandhani",
 };
 
 export default function RootLayout({
@@ -19,7 +21,10 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toaster richColors position="top-center"/>
+      </body>
     </html>
   );
 }
--- src/components/Nav/Profile.tsx
+++ src/components/Nav/Profile.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import {
   DropdownMenu,
@@ -8,28 +9,49 @@ import {
   DropdownMenuLabel,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { SlLogin } from "react-icons/sl";
+import { SlLogin, SlLogout } from "react-icons/sl";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function Profile() {
+  const [user, setUser] = useState(false);
+
+  const handleUser = () => {
+    user
+      ? toast.info("Working on it")
+      : toast.error("Please Login!", {
+          description: "Can't acccess this route without loggin in.",
+        });
+  };
+
   return (
     <div className="flex items-center gap-2">
       <DropdownMenu>
         <DropdownMenuTrigger>
           <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>CN</AvatarFallback>
+            <AvatarImage src="https://source.unsplash.com/hands-formed-together-with-red-heart-paint-cAtzHUz7Z8g" />
+            <AvatarFallback>DP</AvatarFallback>
           </Avatar>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuLabel>User Name</DropdownMenuLabel>
+          <DropdownMenuLabel>{user ? "John Doe" : "No User"}</DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <Link href="/login">
+          {user ? (
             <DropdownMenuItem className="gap-2">
-              Login <SlLogin />
+              Logout <SlLogout />
             </DropdownMenuItem>
-          </Link>
-          <DropdownMenuItem>Dashboard</DropdownMenuItem>
-          <DropdownMenuItem>My Donation</DropdownMenuItem>
+          ) : (
+            <Link href="/login">
+              <DropdownMenuItem className="gap-2">
+                Login <SlLogin />
+              </DropdownMenuItem>
+            </Link>
+          )}
+          <DropdownMenuItem onClick={() => setUser(!user)}>
+            Change State
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={handleUser}>Dashboard</DropdownMenuItem>
+          <DropdownMenuItem onClick={handleUser}>My Donation</DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
