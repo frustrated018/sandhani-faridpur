546bc977e0eb37917c5fd7c6cc61b3e6bd1af4a2
frustrated018 <niloyd018@gmail.com>
2024-03-12 00:03:07 +0600

    Made the navbar responsive

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -9,7 +9,16 @@ import {
   TbMail,
 } from "react-icons/tb";
 import { RiGalleryLine } from "react-icons/ri";
-import Profile from "./Profile";
+import Profile, { MobileProfile } from "./Profile";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 const links = [
   { href: "/request", label: "Request", icon: TbDropletPlus },
@@ -26,11 +35,13 @@ export default function Navbar() {
         <section className="flex items-center justify-between py-4">
           <Link href="/" className="flex items-center gap-2">
             <Image src="/logo-small.svg" alt="logo" height={50} width={50} />
-            <h3 className="text-2xl font-semibold">Sandhani, Faridpur</h3>
+            <h3 className="text-xl font-semibold xl:text-2xl">
+              Sandhani, Faridpur
+            </h3>
           </Link>
           {/* Links */}
           <section className="flex items-center gap-2">
-            <div className="flex items-center gap-2">
+            <div className="hidden items-center gap-2 lg:flex">
               {links.map((link) => (
                 <Link
                   href={link.href}
@@ -46,8 +57,43 @@ export default function Navbar() {
               ))}
             </div>
 
-            <div>
+            <div className="hidden lg:block">
               <Profile />
+            </div>
+
+            {/* Mobile Nav */}
+            <div className="block lg:hidden">
+              <Sheet>
+                <SheetTrigger>
+                  <HamburgerMenuIcon className="size-6" />
+                </SheetTrigger>
+                <SheetContent>
+                  <SheetHeader>
+                    <SheetTitle className="text-xl">
+                      Sandhani, Faridpur
+                    </SheetTitle>
+                  </SheetHeader>
+
+                  <div className="mt-5">
+                    <MobileProfile />
+                  </div>
+                  <div className="mt-5 grid grid-cols-2 gap-2">
+                    {links.map((link) => (
+                      <Link
+                        href={link.href}
+                        key={link.href}
+                        className={buttonVariants({
+                          variant: "outline",
+                          className: "items-center gap-2",
+                        })}
+                      >
+                        {link.icon ? <link.icon /> : null}
+                        {link.label}
+                      </Link>
+                    ))}
+                  </div>
+                </SheetContent>
+              </Sheet>
             </div>
           </section>
         </section>
--- src/components/Nav/Profile.tsx
+++ src/components/Nav/Profile.tsx
@@ -12,6 +12,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { SlLogin, SlLogout } from "react-icons/sl";
 import { useState } from "react";
 import { toast } from "sonner";
+import { Button } from "../ui/button";
 
 export default function Profile() {
   const [user, setUser] = useState(false);
@@ -55,5 +56,49 @@ export default function Profile() {
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
+  );
+}
+
+//! Mobile version of profile
+
+export function MobileProfile() {
+  const [user, setUser] = useState(false);
+
+  const handleUser = () => {
+    user
+      ? toast.info("Working on it")
+      : toast.error("Please Login!", {
+          description: "Can't acccess this route without loggin in.",
+        });
+  };
+
+  return (
+    // TODO: Turn these buttons into sheet trigger to close the sheet upon click
+    <>
+      <div className="flex justify-center items-center my-5">
+        <Avatar>
+          <AvatarImage src="https://source.unsplash.com/hands-formed-together-with-red-heart-paint-cAtzHUz7Z8g" />
+          <AvatarFallback>DP</AvatarFallback>
+        </Avatar>
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        <Button variant="outline" onClick={() => setUser(!user)}>
+          Change State
+        </Button>
+        <Button variant="outline" onClick={handleUser}>
+          Dashboard
+        </Button>
+        <Button variant="outline" onClick={handleUser}>
+          My Donation
+        </Button>
+        {user ? (
+          <Button className="gap-2">
+            Logout <SlLogout />
+          </Button>
+        ) : (
+          <Button>Login</Button>
+        )}
+      </div>
+    </>
   );
 }
