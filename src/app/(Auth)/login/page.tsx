49e1b95ea6e4e8ff3256266246792fa9ce4071ca
frustrated018 <niloyd018@gmail.com>
2024-03-12 22:48:43 +0600

    Updates
    Added a card footer that leads to signup page
    updated the card size from sm to 400px
    added a google button

--- src/app/(Auth)/login/page.tsx
+++ src/app/(Auth)/login/page.tsx
@@ -1,11 +1,20 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
   return (
-    <Card className="w-full max-w-sm bg-transparent text-white backdrop-blur-lg">
+    <Card className="w-full max-w-[400px] bg-transparent text-white backdrop-blur-lg">
       <CardHeader>
         <CardTitle className="text-center text-2xl">Login</CardTitle>
       </CardHeader>
@@ -19,15 +28,36 @@ const Login = () => {
             <Label htmlFor="password">Password:</Label>
             <Input />
           </div>
-          <Button
-            type="submit"
-            variant="outline"
-            className="text-secondary-foreground"
-          >
-            Login with Email
-          </Button>
+          <section className="flex items-center gap-2">
+            <Button
+              type="submit"
+              variant="outline"
+              className="text-secondary-foreground"
+            >
+              Login with Email
+            </Button>
+            <span className="font-bold">Or</span>
+            <Button
+              className="items-center gap-1 text-secondary-foreground"
+              variant="outline"
+            >
+              <FcGoogle size={20} />
+              Login with Goolge
+            </Button>
+          </section>
         </form>
       </CardContent>
+      <CardFooter>
+        <div className="flex items-center gap-1">
+          Don&apos;t have an account? Make one
+          <Link
+            href="/sign-up"
+            className="flex items-center underline hover:font-semibold hover:text-white"
+          >
+            Here <ArrowTopRightIcon />
+          </Link>
+        </div>
+      </CardFooter>
     </Card>
   );
 };
